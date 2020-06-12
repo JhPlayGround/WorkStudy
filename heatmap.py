f5faabceea719c7c3c8f5e4ac48510195ce6e814
juho1210 <wngh9512@gmail.com>
2020-06-12 23:25:31 +0900

    한글화, 범례 가로 , 결과 html

--- heatmap.py
+++ heatmap.py
@@ -5,14 +5,14 @@ import seaborn as sns
 
 # 데이터
 flights = sns.load_dataset('flights')
-source = pd.DataFrame({'Year': flights['year'], 'Month': flights['month'],'Passengers': flights['passengers']})
+source = pd.DataFrame({'연도': flights['year'], '월': flights['month'],'탑승자 수': flights['passengers']})
 
 #그리기
 chart = alt.Chart(source, title="연도 경과에 따른 월별 비행기 탑승자 수").mark_rect().encode(
-    x='Year:O',
-    y='Month:O',
-    color=alt.Color('Passengers:Q', scale=alt.Scale(scheme="inferno")),
-    tooltip=[alt.Tooltip('Year:O', title='연도'), alt.Tooltip('Month:O', title='월'), alt.Tooltip('Passengers:Q', title='탑승자 수')]
+    alt.X('연도:O', scale=alt.Scale(paddingInner=0)),
+    alt.Y('월:O', scale=alt.Scale(paddingInner=0)),
+    color=alt.Color('탑승자 수:Q', scale=alt.Scale(scheme="greenblue"), legend=alt.Legend(direction='horizontal')),
+    tooltip=[alt.Tooltip('연도:O', title='연도'), alt.Tooltip('월:O', title='월'), alt.Tooltip('탑승자 수:Q', title='탑승자 수')],
 ).properties(width=550, height=300)
 
 #저장
