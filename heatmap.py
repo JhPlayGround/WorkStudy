d5cb6ec3c015916c291ae4bc8006f35fd439bbf4
juho1210 <wngh9512@gmail.com>
2020-06-12 22:43:46 +0900

    색상 변경 , 툴팁 추가, 그래프 사이즈 추가

--- heatmap.py
+++ heatmap.py
@@ -7,13 +7,13 @@ import seaborn as sns
 flights = sns.load_dataset('flights')
 source = pd.DataFrame({'Year': flights['year'], 'Month': flights['month'],'Passengers': flights['passengers']})
 
-
 #그리기
-chart = alt.Chart(source).mark_rect().encode(
+chart = alt.Chart(source, title="연도 경과에 따른 월별 비행기 탑승자 수").mark_rect().encode(
     x='Year:O',
     y='Month:O',
-    color='Passengers:Q'
-)
+    color=alt.Color('Passengers:Q', scale=alt.Scale(scheme="inferno")),
+    tooltip=[alt.Tooltip('Year:O', title='연도'), alt.Tooltip('Month:O', title='월'), alt.Tooltip('Passengers:Q', title='탑승자 수')]
+).properties(width=550, height=300)
 
 #저장
 chart.save('heatmap.html')
