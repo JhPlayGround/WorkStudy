b36cbcd52821e103534a48b52f5a3beef702e8d1
juho1210 <wngh9512@gmail.com>
2020-06-14 12:21:24 +0900

    특정 숫자 이상일 경우 색상 변경 추가

--- bar.py
+++ bar.py
@@ -6,7 +6,12 @@ source = data.wheat()
 
 chart = alt.Chart(source).mark_bar().encode(
     x='year:O',
-    y="wheat:Q"
+    y="wheat:Q",
+    color=alt.condition(
+        alt.datum.wheat >= 50,
+        alt.value('orange'),    
+        alt.value('steelblue')  
+    )
 ).properties(width=1200, height=600)
 
 
