4c04338b9a5d6e8591a4219ae8172f26c6f7a659
juho1210 <wngh9512@gmail.com>
2020-06-23 18:33:54 +0900

    히트맵 example 커스터마이징 요소 다 추가 완료

--- heatmap.py
+++ heatmap.py
@@ -16,19 +16,21 @@ from vega_datasets import data
 
 import seaborn as sns
 flights = sns.load_dataset('flights')
-
 source = pd.DataFrame({'Year': flights.year, 'Month':flights.month, 'Passengers':flights.passengers})
 
+#기본 히트맵
 base = alt.Chart(source, title="연도 경과에 따른 월별 비행기 탑승 고객수").encode(
     x='Year:O', y='Month:O'
     
 ).properties(width=550, height =300)
 
+#툴팁, 색상
 heatmap = base.mark_rect().encode(
     color=alt.Color('Passengers:Q', scale=alt.Scale(scheme="viridis"), legend=alt.Legend(direction='horizontal')),
     tooltip=[alt.Tooltip('Year:O', title='연도 '), alt.Tooltip('Month:O', title='월'), alt.Tooltip('Passengers:Q', title='탑승객 수')]
 )
 
+#텍스트 부분
 text = base.mark_text(baseline = 'middle').encode(
     text = 'Passengers',
     color=alt.condition(
@@ -38,6 +40,7 @@ text = base.mark_text(baseline = 'middle').encode(
     )
 )
 
+# 툴팁, 색상 히트맵 + 텍스트
 chart = (heatmap + text)
 
 chart.save('Simple Heatmap.html')
